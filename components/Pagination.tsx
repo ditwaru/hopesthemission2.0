e18c5216ea543445115cf12d761e226fdedee604
Daniel Itwaru <ditwaru@siterx.com>
2023-06-28 21:59:00 -0400

    commenting out font awesome

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,9 +1,6 @@
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLongArrowAltLeft,
-  faLongArrowAltRight,
-} from '@fortawesome/free-solid-svg-icons';
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltLeft, faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   type: string;
@@ -15,36 +12,20 @@ export const Pagination = ({ type, pageNumbers, currentPage }: Props) => {
   return (
     <nav className="flex justify-between mx-5">
       <Link href={`/${type}/page/${currentPage - 1}`}>
-        <a
-          className={currentPage === 1 ? `pointer-events-none opacity-50` : ''}
-        >
-          <FontAwesomeIcon icon={faLongArrowAltLeft} />
+        <a className={currentPage === 1 ? `pointer-events-none opacity-50` : ""}>
+          {/* <FontAwesomeIcon icon={faLongArrowAltLeft} /> */}
         </a>
       </Link>
 
-      {Array.from({ length: pageNumbers }, (bruh, i) => i + 1).map(
-        (pageNumber) => (
-          <Link key={pageNumber} href={`/${type}/page/${pageNumber}`}>
-            <a
-              className={
-                currentPage === pageNumber
-                  ? 'underline text-blue-500 text-xl'
-                  : ''
-              }
-            >
-              {pageNumber}
-            </a>
-          </Link>
-        )
-      )}
+      {Array.from({ length: pageNumbers }, (bruh, i) => i + 1).map((pageNumber) => (
+        <Link key={pageNumber} href={`/${type}/page/${pageNumber}`}>
+          <a className={currentPage === pageNumber ? "underline text-blue-500 text-xl" : ""}>{pageNumber}</a>
+        </Link>
+      ))}
 
       <Link href={`/${type}/page/${currentPage + 1}`}>
-        <a
-          className={
-            currentPage === pageNumbers ? `pointer-events-none opacity-50` : ''
-          }
-        >
-          <FontAwesomeIcon icon={faLongArrowAltRight} />
+        <a className={currentPage === pageNumbers ? `pointer-events-none opacity-50` : ""}>
+          {/* <FontAwesomeIcon icon={faLongArrowAltRight} /> */}
         </a>
       </Link>
     </nav>
