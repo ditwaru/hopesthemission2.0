5048df3b335a94aa1436a12580bbf95bb03547fd
Daniel Itwaru <ditwaru@siterx.com>
2023-06-28 22:00:34 -0400

    commenting out the other font awesome

--- components/AdminPagination.tsx
+++ components/AdminPagination.tsx
@@ -1,9 +1,6 @@
-import { Dispatch, SetStateAction, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLongArrowAltLeft,
-  faLongArrowAltRight,
-} from '@fortawesome/free-solid-svg-icons';
+import { Dispatch, SetStateAction, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltLeft, faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   contents: {}[];
@@ -14,13 +11,9 @@ interface Props {
 export const AdminPagination = ({ contents, amount, setPostIndex }: Props) => {
   const [pageIndex, setPageIndex] = useState(1);
   const totalContent: number = contents.length;
-  const highestPageNumber =
-    totalContent < amount ? 1 : Math.ceil(totalContent / amount);
+  const highestPageNumber = totalContent < amount ? 1 : Math.ceil(totalContent / amount);
 
-  const pagination = Array.from(
-    { length: highestPageNumber },
-    (bruh, i) => i + 1
-  );
+  const pagination = Array.from({ length: highestPageNumber }, (bruh, i) => i + 1);
   const pageClickHandler = (i: number) => {
     setPostIndex(amount * i - amount); // 0 indexed
     setPageIndex(i); // 1 indexed
@@ -29,33 +22,29 @@ export const AdminPagination = ({ contents, amount, setPostIndex }: Props) => {
   return (
     <nav className="flex justify-between">
       <button
-        className={`ml-5 ${
-          pageIndex === 1 ? 'opacity-50 point-events-none' : ''
-        }`}
+        className={`ml-5 ${pageIndex === 1 ? "opacity-50 point-events-none" : ""}`}
         onClick={() => {
           pageIndex > 1 && pageClickHandler(pageIndex - 1);
         }}
       >
-        <FontAwesomeIcon icon={faLongArrowAltLeft} />
+        {/* <FontAwesomeIcon icon={faLongArrowAltLeft} /> */}
       </button>
       {pagination.map((i) => (
         <button
           key={i}
           onClick={() => pageClickHandler(i)}
-          className={pageIndex === i ? 'underline text-blue-500 text-xl' : ''}
+          className={pageIndex === i ? "underline text-blue-500 text-xl" : ""}
         >
           {i}
         </button>
       ))}
       <button
-        className={`mr-5 ${
-          pageIndex === highestPageNumber ? 'opacity-50 point-events-none' : ''
-        }`}
+        className={`mr-5 ${pageIndex === highestPageNumber ? "opacity-50 point-events-none" : ""}`}
         onClick={() => {
           pageIndex < highestPageNumber && pageClickHandler(pageIndex + 1);
         }}
       >
-        <FontAwesomeIcon icon={faLongArrowAltRight} />
+        {/* <FontAwesomeIcon icon={faLongArrowAltRight} /> */}
       </button>
     </nav>
   );
