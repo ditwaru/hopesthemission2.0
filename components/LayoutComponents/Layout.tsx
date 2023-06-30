038ee7fecd6a28ed8d986d0f2b9520d976a6f479
Daniel Itwaru <ditwaru@siterx.com>
2023-06-30 14:53:15 -0400

    escaping apostraphes

--- components/LayoutComponents/Layout.tsx
+++ components/LayoutComponents/Layout.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Navbar } from 'components/LayoutComponents/Navbar';
-import { Footer } from 'components/LayoutComponents/Footer';
-import background from '/public/marble-background.webp';
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { Navbar } from "components/LayoutComponents/Navbar";
+import { Footer } from "components/LayoutComponents/Footer";
+import background from "/public/marble-background.webp";
 
 type Props = {
   children?: ReactNode;
@@ -14,7 +14,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="relative min-h-screen">
       <Head>
-        <title>Hope's the Mission</title>
+        <title>Hope&apos;s the Mission</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
@@ -23,12 +23,7 @@ const Layout = ({ children }: Props) => {
       </header>
       <main className="font-nanumGothic pt-24 pb-16 w-11/12 max-w-lg mx-auto">
         <div className="w-screen absolute top-0 left-0 bottom-0 -z-10 ">
-          <Image
-            src={background}
-            layout="fill"
-            objectPosition="center"
-            priority
-          />
+          <Image src={background} layout="fill" objectPosition="center" priority />
         </div>
         {children}
       </main>
--- pages/sponsor.tsx
+++ pages/sponsor.tsx
@@ -1,13 +1,10 @@
 const Sponsor = () => {
   return (
     <div>
-      <h1 className="text-5xl text-center font-rockSalt mt-5">
-        Sponsor a missionary
-      </h1>
+      <h1 className="text-5xl text-center font-rockSalt mt-5">Sponsor a missionary</h1>
       <h3 className="text-center font-nanumPen text-4xl  my-5">
-        Have you ever wanted to go on a mission trip, but circumstances won't
-        allow it? This is your chance to bring hope to different parts of the
-        world by financially supporting one of these missionaries.
+        Have you ever wanted to go on a mission trip, but circumstances won&apos;t allow it? This is your chance to
+        bring hope to different parts of the world by financially supporting one of these missionaries.
       </h3>
     </div>
   );
